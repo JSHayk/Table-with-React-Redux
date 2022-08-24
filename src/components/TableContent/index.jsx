ff731d8570150ab3457401731cbf20243d2fc801
JSHayk <jshaykdeveloper@gmail.com>
2022-08-24 15:06:37 +0400

    changed search logic

--- src/components/TableContent/index.jsx
+++ src/components/TableContent/index.jsx
@@ -1,40 +1,43 @@
-import { useSelector, useDispatch } from "react-redux"
+import { useSelector, useDispatch } from "react-redux";
 import { deletePerson } from "../../slices/tableSlice";
-import { MdDelete } from "react-icons/md"
+import { MdDelete } from "react-icons/md";
 
 const TableContent = () => {
-  const {persons} = useSelector(state => state.table);
+  const { persons } = useSelector((state) => state.table);
   const dispatch = useDispatch();
 
-    return (
-        <div className='table-content'>
-            <ul className="table-content-titles">
-                <li>ID</li>
-                <li>Name</li>
-                <li>City</li>
-                <li>Company Name</li>
-                <li>Phone</li>
-            </ul>
-            <ul className="table-content-persons">
-                {
-                    persons.map(item => {
-                        const { id, name, address, company, phone } = item;
-                        const { city } = address;
-                        return (
-                            <section key={ id }>
-                                <li>{ id }</li>
-                                <li>{ name }</li>
-                                <li>{ city }</li>
-                                <li>{ company.name }</li>
-                                <li>{ phone }</li>
-                                <li onClick={() => dispatch(deletePerson(id))} className="person-delete"><MdDelete /></li>
-                            </section> 
-                        )
-                    })
-                }
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="table-content">
+      <ul className="table-content-titles">
+        <li>ID</li>
+        <li>Name</li>
+        <li>City</li>
+        <li>Company Name</li>
+        <li>Phone</li>
+      </ul>
+      <ul className="table-content-persons">
+        {persons.map((item) => {
+          const { id, name, address, company, phone } = item;
+          const { city } = address;
+          return (
+            <section key={id}>
+              <li>{id}</li>
+              <li>{name}</li>
+              <li>{city}</li>
+              <li>{company.name}</li>
+              <li>{phone}</li>
+              <li
+                onClick={() => dispatch(deletePerson(id))}
+                className="person-delete"
+              >
+                <MdDelete />
+              </li>
+            </section>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default TableContent
+export default TableContent;
--- src/components/TableMenu/index.jsx
+++ src/components/TableMenu/index.jsx
@@ -1,30 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { searchPerson } from "../../slices/tableSlice"
+import { searchPerson } from "../../slices/tableSlice";
 
 const TableMenu = () => {
   const [val, setVal] = useState("");
   const dispatch = useDispatch();
   useEffect(() => {
-    if (val.trim()) {
-      dispatch(searchPerson(val.toLowerCase()));
-    }
+    dispatch(searchPerson(val.toLowerCase()));
   }, [val, dispatch]);
 
   const changeHandleInput = (event) => {
     const value = event.target.value;
-    if (!value.match(/^\s*$/)) {
-      setVal(value);
-    }
-  }
+    setVal(value);
+  };
 
   return (
     <div className="table-menu">
-      <section className='table-menu-search-container'>
-        <input onChange={changeHandleInput} type="text" placeholder='Search Person with name' />
+      <section className="table-menu-search-container">
+        <input
+          onChange={changeHandleInput}
+          type="text"
+          placeholder="Search Person with name"
+        />
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default TableMenu
+export default TableMenu;
