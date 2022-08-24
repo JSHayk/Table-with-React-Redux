efbda273a73c0225c0f391b430a1145fade62124
JSHayk <jshaykdeveloper@gmail.com>
2022-08-24 14:55:25 +0400

    changed delete person logic

--- src/slices/tableSlice.js
+++ src/slices/tableSlice.js
@@ -1,31 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    persons: [],
-    personsClone: [],
-    isLoad: true
-}
+  persons: [],
+  personsClone: [],
+  isLoad: true,
+};
 
 const tableSlice = createSlice({
-    name: "table",
-    initialState,
-    reducers: {
-        setData(state, { payload }) {
-            state.isLoad = false
-            state.persons = payload
-            state.personsClone = payload
-        },
-        deletePerson(state, { payload }) {
-            const deletedData = state.persons.filter(item => item.id !== payload);
-            state.persons = deletedData
-            state.personsClone = deletedData
-        },
-        searchPerson(state, { payload }) {
-            const searchedData = state.personsClone.filter(item => item.name.toLowerCase().includes(payload));
-            state.persons = searchedData
-        }
-    }
+  name: "table",
+  initialState,
+  reducers: {
+    setData(state, { payload }) {
+      state.isLoad = false;
+      state.persons = payload;
+      state.personsClone = payload;
+    },
+    deletePerson(state, { payload }) {
+      state.persons = state.persons.filter((item) => item.id !== payload);
+      state.personsClone = state.personsClone.filter(
+        (item) => item.id !== payload
+      );
+    },
+    searchPerson(state, { payload }) {
+      const searchedData = state.personsClone.filter((item) =>
+        item.name.toLowerCase().includes(payload)
+      );
+      state.persons = searchedData;
+    },
+  },
 });
 
-export const { setData, deletePerson, searchPerson } = tableSlice.actions
-export default tableSlice.reducer
+export const { setData, deletePerson, searchPerson } = tableSlice.actions;
+export default tableSlice.reducer;
